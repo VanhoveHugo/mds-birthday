d3bf88133dfe58b37e7158fd6f49ddc49c0f98c5
VanhoveHugo <vanhovehugo1@gmail.com>
2023-06-07 15:19:03 +0200

    Create Layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,20 @@
+import { useEffect } from 'react';
+
 export default function Home() {
+  const colors = ['#DF80AC', '#579FF4', '#FCB325', '#098E27'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-      </div>
-    </main>
+    <>
+      <header>
+        <h1>Citation et anniversaires</h1>
+        <p>date</p>
+      </header>
+      <main style={{ backgroundColor: randomColor }}>
+        <div className="name">
+        </div>
+        <div className="text"></div>
+      </main>
+    </>
   )
 }
