546670f4d021f732904dd77d522d90c21fbe3fb3
Boucher Killian <boucherkillian@MacBook-Air-de-Boucher.local>
2023-06-08 11:14:36 +0200

    add date

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,30 @@
-import { useEffect } from 'react';
+'use client'
+
+import { useEffect, useState } from 'react';
+import {DateTime} from 'luxon';
 
 export default function Home() {
   const colors = ['#DF80AC', '#579FF4', '#FCB325', '#098E27'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const [date, setDate] = useState(DateTime.now());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+        setDate(DateTime.now());
+    }, 60000);
+    return function cleanup() {
+        clearInterval(timer);
+    }
+  }
+);
 
   return (
     <>
-      <header>
+      <header>  
         <h1>Citation et anniversaires</h1>
-        <p>date</p>
+        <p>{date.setLocale('fr').toFormat("cccc LL LLLL y T"  )}</p>
       </header>
       <main style={{ backgroundColor: randomColor }}>
         <div className="name">
